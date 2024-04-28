f07ea14005b9622dbc6c0c50b52ae0bd4a6f8954
Selvakumar Boominathan <selvans2k@gmail.com>
2024-04-29 00:38:09 +0530

    styles added to the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
 import StorefrontSharpIcon from '@mui/icons-material/StorefrontSharp';
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import './style.css'
 
 const header = () => {
   return (
-    <div>
+    <div className="header">
 
       <div className="header_logo">
         <StorefrontSharpIcon className='header_logoImage'  fontSize="large"/>
-        <h2 className='header_logoTitle'>Eshop</h2>
+        <h2 className='header_logoTitle'>E shop</h2>
       </div>
 
       <div className="header_search">
+        <SearchSharpIcon className="header_searchIcon"/>
         <input type="text" className="header_input" />
 
 
@@ -30,9 +33,9 @@ const header = () => {
           <span className="nav_Item2">Shop</span>
         </div>
 
-        <div className="nav_Item">
-        <ShoppingCartSharpIcon  fontSize='large'/>
-          <span className="nav_Item2">0</span>
+        <div className="nav_ItemBasket">
+        <ShoppingCartSharpIcon className="itembasket"  fontSize='large'/>
+          <span className="nav_Item2 nav_basketCount">0</span>
         </div>
 
       </div>
