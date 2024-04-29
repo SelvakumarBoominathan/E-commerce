28f0dbb129da6de7d7bf86d1cea929cc86de0ba4
Selvakumar Boominathan <selvans2k@gmail.com>
2024-04-29 23:24:28 +0530

    Added home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Header from "./components/header.jsx"
+import Header from "./components/header.jsx";
+import Home from "./components/home.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header/>
+     <Home/>
     </>
   )
 }
