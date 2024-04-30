05b540bed6c1106ad2ac7556e8c8f2f98989d966
Selvakumar Boominathan <selvans2k@gmail.com>
2024-05-01 01:41:00 +0530

    added updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@
 import './App.css';
 import Header from "./components/header.jsx";
 import Home from "./components/home.jsx";
-import Links from "./components/Links.jsx"
-import Parentbody from "./components/Parentbody.jsx"
-
+import Links from "./components/Links.jsx";
+import Parentbody from "./components/Parentbody.jsx";
+import { useState } from 'react';
 
 
 function App() {
 
+    const [valueFromLinks, setValueFromLinks] = useState(null);
+  
+    // Callback function to receive the value from child component
+    const handleValueFromLink = (value) => {
+      setValueFromLinks(value);
+    };
+  
+
   return (
     <div>
           <Header/>
-          <Links/>  
+          <Links valueFromParent={valueFromLinks}/>  
           <Home/>
-          <Parentbody/>
+          <Parentbody onValueFromLink={handleValueFromLink}/>
           
      </div>
   )
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import {BrowserRouter, Link} from 'react-router-dom';
 import './style.css';
+import Parentbody from './Parentbody';
 
 
 // let category_Array = ["All", "Electronics", "Fashion", "Appliences", "Mobiles"]
 
 const Links = () => {
+
+const [selectedCategory, Setselectedcategory] = useState("All");
+
+const handleLinkClick = (category) => {
+  Setselectedcategory(category)
+}
+
+
 return(
   <div className="navbar_container">
 <BrowserRouter>
-      <Link to="/" className='btn_link'>All</Link>
-      <Link to="/electronics" className='btn_link'>Electronics</Link>
-      <Link to="/fashion" className='btn_link'>Fashion</Link>
-      <Link to="/appliances" className='btn_link'>Appliances</Link>
-      <Link to="/mobiles" className='btn_link'>Mobiles</Link>
+      <Link to="/" className='btn_link' onClick={() => handleLinkClick("All")}>All</Link>
+      <Link to="/electronics" className='btn_link' onClick={() => handleLinkClick("Electronics")}>Electronics</Link>
+      <Link to="/fashion" className='btn_link' onClick={() => handleLinkClick("Fashion")}>Fashion</Link>
+      <Link to="/appliances" className='btn_link' onClick={() => handleLinkClick("Appliances")}>Appliances</Link>
+      <Link to="/mobiles" className='btn_link' onClick={() => handleLinkClick("Mobiles")}>Mobiles</Link>
+      {/* <Parentbody selectedCategory={selectedCategory}/> */}
 </BrowserRouter>
 </div>
 )
